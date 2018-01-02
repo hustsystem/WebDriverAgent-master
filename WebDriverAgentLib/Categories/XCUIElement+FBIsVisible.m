daa39ea7003b494c2bfa4ac23ba40c680970f1de
shuhua <342092138@qq.com>
2018-01-02 11:30:13 +0800

    Initial commit

---
+++ WebDriverAgentLib/Categories/XCUIElement+FBIsVisible.m
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import "XCUIElement+FBIsVisible.h"
+
+#import "FBApplication.h"
+#import "FBConfiguration.h"
+#import "FBMathUtils.h"
+#import "FBXCodeCompatibility.h"
+#import "XCElementSnapshot+FBHelpers.h"
+#import "XCUIElement+FBUtilities.h"
+#import "XCTestPrivateSymbols.h"
+#import <XCTest/XCUIDevice.h>
+#import "XCElementSnapshot+FBHitPoint.h"
+
+@implementation XCUIElement (FBIsVisible)
+
+- (BOOL)fb_isVisible
+{
+  return self.fb_lastSnapshot.fb_isVisible;
+}
+
+@end
+
+@implementation XCElementSnapshot (FBIsVisible)
+
+- (BOOL)fb_isVisible
+{
+  CGRect frame = self.frame;
+  if (CGRectIsEmpty(frame)) {
+    return NO;
+  }
+  if ([FBConfiguration shouldUseTestManagerForVisibilityDetection]) {
+    return [(NSNumber *)[self fb_attributeValue:FB_XCAXAIsVisibleAttribute] boolValue];
+  }
+  CGRect appFrame = [self fb_rootElement].frame;
+  CGSize screenSize = FBAdjustDimensionsForApplication(appFrame.size, (UIInterfaceOrientation)[XCUIDevice sharedDevice].orientation);
+  CGRect screenFrame = CGRectMake(0, 0, screenSize.width, screenSize.height);
+  if (!CGRectIntersectsRect(frame, screenFrame)) {
+    return NO;
+  }
+  CGPoint midPoint = [self.suggestedHitpoints.lastObject CGPointValue];
+  XCElementSnapshot *hitElement = [self hitTest:midPoint];
+  if (self == hitElement || [self._allDescendants.copy containsObject:hitElement]) {
+    return YES;
+  }
+  if (CGRectContainsPoint(appFrame, self.fb_hitPoint)) {
+    return YES;
+  }
+  for (XCElementSnapshot *elementSnapshot in self.children.copy) {
+    if (elementSnapshot.fb_isVisible) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+@end
